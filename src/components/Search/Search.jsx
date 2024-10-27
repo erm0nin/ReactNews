f8b05f401576d17c96eb581249b0355aa941d1f2
dima <ermonindima0@gmail.com>
2024-10-27 03:55:22 +0300

    add web

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 const Search = ({ keywords, setKeywords }) => {
   return (
     <div className={styles.search}>
-      <input placeholder='text' type="text" value={keywords} onChange={(e) => setKeywords(e.target.value)} className={styles.input} />
+      <input placeholder='Search' type="text" value={keywords} onChange={(e) => setKeywords(e.target.value)} className={styles.input} />
     </div>
   )
 }
